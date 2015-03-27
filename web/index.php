78147b8077232ac65d3a7912455f33a4d6014e4b
Tim Otten <totten@civicrm.org>
2015-03-26 19:55:17 -0700

    index.php - Add endpoint "/apps" for testing the app-browser. Comments. Update composer.

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@ $app['config'] = function () {
   return new \Civi\Cxn\App\AdhocConfig();
 };
 
+// OPTIONAL: Provide a nice endpoint for enterprising web surfers.
 $app->get('/', function () use ($app) {
   header('Content-type: text/plain');
 
@@ -19,15 +20,35 @@ $app->get('/', function () use ($app) {
   return $appMeta['desc'];
 });
 
+// OPTIONAL: Facilitate testing by publishing this
+// app's metadata at a public URL.
 $app->get('/cxn/metadata.json', function () use ($app) {
   header('Content-Type: application/javascript');
 
   /** @var \Civi\Cxn\App\AdhocConfig $config */
   $config = $app['config'];
-  $appMeta = $config->getMetadata();
-  return json_encode($appMeta);
+  return json_encode($config->getMetadata());
 });
 
+// OPTIONAL: Facilitate testing by publishing a feed of all
+// apps.
+$app->get('/cxn/apps', function () use ($app) {
+  header('Content-Type: ' . Constants::MIME_TYPE);
+
+  /** @var \Civi\Cxn\App\AdhocConfig $config */
+  $config = $app['config'];
+  $appMeta = $config->getMetadata();
+  $message = new \Civi\Cxn\Rpc\Message\AppMetasMessage(
+    $appMeta['appCert'],
+    $config->getKeyPair(),
+    array($config->getId() => $config->getMetadata())
+  );
+
+  $message->send();
+  exit();
+});
+
+// REQUIRED: Provide an endpoint for processing registrations.
 $app->post('/cxn/register', function () use ($app) {
   /** @var \Civi\Cxn\App\AdhocConfig $config */
   $config = $app['config'];
@@ -35,7 +56,8 @@ $app->post('/cxn/register', function () use ($app) {
   header('Content-type: ' . Constants::MIME_TYPE);
   $server = new \Civi\Cxn\Rpc\RegistrationServer($config->getMetadata(), $config->getKeyPair(), $config->getCxnStore());
   $server->setLog($config->getLog('RegistrationServer'));
-  $server->handleAndRespond(file_get_contents('php://input'));
+  $server->handle(file_get_contents('php://input'))->send();
+  exit();
 });
 
 $app->error(function ($e) use ($app) {
